8f4a2ee226fed5849f3d8602b5056ddf1c0fe54a
loriful <tiptonica@gmail.com>
2022-03-19 14:44:04 -0400

    updated port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { animals } = require('./data/animals');
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 function filterByQuery(query, animalsArray) {
     
@@ -40,8 +41,8 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
-    app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+    app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
 
 
