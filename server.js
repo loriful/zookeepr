d6073316999e4f64332aecce36ebc5b991f2de4b
loriful <tiptonica@gmail.com>
2022-03-20 23:54:39 -0400

    front-end routes

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
-app.use(express.urlencoded({extended: true}));
-
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
@@ -51,7 +50,7 @@ function createNewAnimal(body, animalsArray) {
     JSON.stringify({ animals: animalsArray }, null, 2)
   );
   return animal;
-};
+}
 
 function validateAnimal(animal) {
   if (!animal.name || typeof animal.name !== 'string') {
@@ -60,15 +59,14 @@ function validateAnimal(animal) {
   if (!animal.species || typeof animal.species !== 'string') {
     return false;
   }
-  if (!animals.diet || typeof animal.diet !== 'string') {
+  if (!animal.diet || typeof animal.diet !== 'string') {
     return false;
   }
-  if (!animals.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
     return false;
   }
-
   return true;
-};
+}
 
 app.get('/api/animals', (req, res) => {
   let results = animals;
@@ -88,14 +86,31 @@ app.get('/api/animals/:id', (req, res) => {
 });
 
 app.post('/api/animals', (req, res) => {
-    req.body.id = animals.length.toString();
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
 
-    if (!validateAnimal(req.body)) {
-      res.status(400).send('The animal is not properly formatted.');
-    } else {
-      const animal = createNewAnimal(req.body, animals);
-      res.json(animal);
-    }
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '.public/index.html'));
 });
 
 app.listen(PORT, () => {
